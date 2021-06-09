5fa4a9a5afa78c3213b386a41c0116915459789f
AndyConstantinMomita <57625842+AndyConstantinMomita@users.noreply.github.com>
2021-06-09 12:49:13 +0300

    Add files via upload

---
+++ export.php
@@ -0,0 +1,28 @@
+<?php
+
+require("./model/db.php");
+
+$query = "SELECT titlu,popularitate FROM retete ORDER BY popularitate DESC;";
+$result =  mysqli_query($conn, $query);
+if (!$result) {
+    exit(mysqli_error($con));
+}
+
+$retete = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $retete[] = $row;
+    }
+}
+
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=clasament_retete.csv');
+$output = fopen('php://output', 'w');
+fputcsv($output, array('Titlu', 'Popularitate'));
+
+if (count($retete) > 0) {
+    foreach ($retete as $row) {
+        fputcsv($output, $row);
+    }
+}
+?>
